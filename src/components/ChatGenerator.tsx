f1af8969e70d97832cd1b63f89ec050f20c9f304
Tfull <24rb1022@studemt.gs.chiba-u.jp>
2025-10-31 19:16:12 +0900

    feat: チャットボットのURLを更新

--- src/components/ChatGenerator.tsx
+++ src/components/ChatGenerator.tsx
@@ -4,7 +4,7 @@ const ChatGenerator: React.FC = () => {
   return (
     <div style={{ width: '100%', height: '100%' }}>
       <iframe
-        src="https://udify.app/chatbot/yexkt33Z2L7GPf68"
+        src="https://chiba-u-ai25-g4.xvps.jp/chatbot/1Ft7aa6uABv76n8J"
         style={{ width: '100%', height: '100%', minHeight: '700px' }}
         frameBorder="0"
         allow="microphone"
